805aaab4683db5921e436097be03f02a68b9a656
liushuo <liushuozaixian@163.com>
2018-07-18 16:41:02 +0800

    获取state中数据的最基本的写法和mapState的几种不同情况的写法！！！！！！！

--- src/components/component1.vue
+++ src/components/component1.vue
@@ -5,16 +5,22 @@
     </h2>
     NUM：{{count2}} <br>
     <button @click="handleClick">点击我数字加一</button>
+    <br>
+    NUM：{{count3}} <br>
+    NUM：{{count4}} <br>
+    NUM：{{count5}} <br>
+    NUM：{{count.num}} <br>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   name: 'component1',
   components: {},
   data() {
     return {
-      count: {num: 0}
+      countb: {num: 100}
     };
   },
   created () {
@@ -32,8 +38,21 @@ export default {
   },
   computed: {
     count2 () {
+      //      获取state中数据的最基本的写法！！！！！
       return this.$store.state.count
-    }
+    },
+    //    获取state中数据的mapState的几种不同情况的写法！！！！！
+    ...mapState({
+      count3: state => state.count.num,
+      count4: 'count',
+      count5 (state) {
+        return state.count.num + this.countb.num
+      }
+    }),
+    ...mapState([
+      // 映射 this.count 为 store.state.count
+      'count'
+    ])
   }
 }
 </script>
