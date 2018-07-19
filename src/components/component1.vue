c02ae4540b2570e8c23188fd0c80a43933cb3b3f
liushuo <liushuozaixian@163.com>
2018-07-19 13:52:00 +0800

    最基本方法获取getters，和通过mapGetters获取getters（getters就相当于是state中的计算属性！）

--- src/components/component1.vue
+++ src/components/component1.vue
@@ -10,11 +10,16 @@
     NUM：{{count4}} <br>
     NUM：{{count5}} <br>
     NUM：{{count.num}} <br>
+    doneTodos1 : {{doneTodos1[0].id}} <br>
+    doneTodosCount : {{doneTodosCount}} <br>
+    getTodoById : {{getTodoById}} <br>
+    通过mapGetters得到的doneTodos : {{doneTodos}} <br>
+    通过mapGetters得到的改为doneTodos2 : {{doneTodos}} <br>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapGetters} from 'vuex'
 export default {
   name: 'component1',
   components: {},
@@ -52,7 +57,27 @@ export default {
     ...mapState([
       // 映射 this.count 为 store.state.count
       'count'
-    ])
+    ]),
+
+
+    //获取getters中的计算属性的最基本的方法！！！！！！！
+    doneTodos1 () {
+      return this.$store.getters.doneTodos
+    },
+    doneTodosCount () {
+      return this.$store.getters.doneTodosCount
+    },
+    //  通过方法访问你也可以通过让 getter 返回一个函数，
+    // 来实现给 getter 传参。在你对 store 里的数组进行查询时非常有用。
+    getTodoById () {
+      return this.$store.getters.getTodoById(2)
+    },
+    //  通过mapGetters来获取store中的getters的计算属性
+    ...mapGetters(['doneTodos']),
+    //    通过mapGetters来获取store中的getters的计算属性并改名
+    ...mapGetters({
+      doneTodos2: 'doneTodos'
+    })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,35 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+
   state: {
-    count: {num: 0}
+    count: {num: 0},
+    todos: [
+      {id: 1, text: '...', done: true},
+      {id: 2, text: '...', done: false},
+      {id: 2, text: '...', done: false}
+    ]
   },
+
+  //getters相当于是在store中的计算属性（一些复杂的经常用到的属性可以放在这里）！！
+  // Vuex 允许我们在 store 中定义“getter”（可以认为是 store 的计算属性）。
+  // 就像计算属性一样，getter 的返回值会根据它的依赖被缓存起来，
+  // 且只有当它的依赖值发生了改变才会被重新计算。
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    //  Getter 也可以接受其他 getter 作为第二个参数：
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    //  通过方法访问你也可以通过让 getter 返回一个函数，
+    // 来实现给 getter 传参。在你对 store 里的数组进行查询时非常有用。
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id === id);
+    }
+  },
+
   mutations: {
     increment (state) {
       state.count.num++;
